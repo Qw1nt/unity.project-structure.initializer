1415fd6a4f631b30f97572ae55f322c6800edd81
Qw1nt <pasharudnykh3@mail.ru>
2023-09-07 14:00:31 +0500

    [+] Парсер иерархии

---
+++ Editor/Core/Common/HierarchyParser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using ProjectStructure.Initializer.Editor.Core.Interfaces;
+
+namespace ProjectStructure.Initializer.Editor.Core.Common
+{
+    public class HierarchyParser : IHierarchyParser
+    {
+        public void Execute(IEnumerable<Folder> hierarchy, Queue<Folder> result)
+        {
+            foreach (var root in hierarchy)
+                BuildBranch(root, result);
+        }
+        
+        private void BuildBranch(Folder root, Queue<Folder> result)
+        {
+            var target = root;
+            result.Enqueue(target);
+
+            while (target.SubFolders.TryDequeue(out var child))
+            {
+                target = child;
+                result.Enqueue(target);
+
+                while (target.SubFolders.Count == 0 && target.Root != null)
+                    target = target.Root;
+            }
+        }
+    }
+}
---
+++ Editor/Core/Interfaces/IHierarchyParser.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using ProjectStructure.Initializer.Editor.Core.Common;
+
+namespace ProjectStructure.Initializer.Editor.Core.Interfaces
+{
+    public interface IHierarchyParser
+    {
+        void Execute(IEnumerable<Folder> hierarchy, Queue<Folder> result);
+    }
+}
